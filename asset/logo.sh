dcd9c9d00161a8936859f99bb10794e2a3603b84
HUAN WANG <wanghuan_hh@126.com>
2024-10-20 17:00:05 +0800

    Update logo.sh

--- asset/logo.sh
+++ asset/logo.sh
@@ -2,12 +2,14 @@
 
 asset::logo() {
   cat <<\EOF
-                               _
- _ ____      ___ __ ___   __ _| |_
-| '_ \ \ /\ / / '_ ` _ \ / _` | __|  website: http://www.lonxun.com
-| |_) \ V  V /| | | | | | (_| | |_   v1.0.0
-| .__/ \_/\_/ |_| |_| |_|\__,_|\__|  PWkit Usage: http://doc.lonxun.com/PWkit/PWkit.html
+               _      _   _   
+ _ ____      _| | __ (_)_| |__  
+|  _ \ \ /\ / / |/ / | |_   __|   website: http://www.lonxun.com
+| |_) \ V  V /| |\ \ | | | |_     version 3.0.0
+| .__/ \_/\_/ |_| \_\|_|  \__|    PWkit Usage: http://doc.lonxun.com/PWkit
 |_|
+
+A pre- and post-data processing toolkit for PWmat
 
 EOF
 }
